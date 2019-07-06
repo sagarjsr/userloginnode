79802f1e5f35f36ef3d4558418aec02122a1929b
Sagar Kumar <sagargecc@gmail.com>
2019-07-06 20:42:40 +0530

    update

--- server.js
+++ server.js
@@ -1,62 +1,30 @@
-const http = require('http');
 const Joi = require('joi');
 const express = require('express');
+const logger = require('./logger');
+const helmet = require('helmet');
+const config = require('config');
+const courses= require('./routes/courses')
+const home= require('./routes/home')
 
 const app = express();
+app.use('/api/courses', courses)
+app.use('/', home);
+app.set('view engine', 'pug');
+app.set('views', './views')
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(logger);
+app.use(helmet()); 
 
-const courses = [
-    { id: 1, name: 'courses1' },
-    { id: 2, name: 'courses2' },
-    { id: 3, name: 'courses3' }
-]
 
-app.get('/', (req, res) => {
-    res.send('hello sagar!!!!');
-});
+//congiguration 
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
+console.log('Application Name:' + config.get('name'));
+console.log('Application Name:' + config.get('mail.host'));
+ 
 
-app.get('/api/courses/:id', (req, res) => {
 
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('the course not found');
-    res.send(course);
-});
 
-app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.validate(req.body, schema);
-    console.log(result);
-
-    if (!res.body.name || req.body.name.lenght < 3) {
-        res.status(400).send('name is not valid');
-        return;
-    }
-    const course = {
-        id: courses.lenght + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-// const server = http.createServer((req, res) =>{
-//     if (req.url === '/') { 
-//         res.write('hello Sagar');
-//         res.end();
-//     }
-//     if (res.url ==='/api/courses'){
-//         res.write(JSON.stringify([1,2,3]));
-//         res.end();
-//     }
-// });
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () =>
-    console.log(`server started on port  ${port}...export `));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
